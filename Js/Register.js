658e5777a2d2c5fb61ebe8b2882938a92b976b57
Juan Sebastian Torres <111210405+JuanSebastianTorresRozo@users.noreply.github.com>
2024-03-27 11:40:50 -0500

    Add files via upload

---
+++ Js/Register.js
@@ -0,0 +1,7 @@
+const botonRegistrar = document.getElementById('botonEnviar'); // Agrega comillas alrededor de 'botonEnviar'
+
+botonRegistrar.addEventListener('click', mostrarFuncion);
+
+function mostrarFuncion(boton) {
+    alert("holaaa perraaaa");
+}
